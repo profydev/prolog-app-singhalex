ff3c5c9f43d95c42229daee9b90c0b1e35420ab0
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-11-30 16:29:20 -0800

    Project list incorrect status (#7)
    Render the correct color and text on the project badges and implement tests.

--- cypress/e2e/project-list.cy.ts
+++ cypress/e2e/project-list.cy.ts
@@ -1,5 +1,6 @@
-import capitalize from "lodash/capitalize";
 import mockProjects from "../fixtures/projects.json";
+
+import { ProjectStatus } from "@api/projects.types";
 
 describe("Project List", () => {
   beforeEach(() => {
@@ -22,17 +23,25 @@ describe("Project List", () => {
 
     it("renders the projects", () => {
       const languageNames = ["React", "Node.js", "Python"];
+      const statusToText = {
+        [ProjectStatus.info]: "Stable",
+        [ProjectStatus.warning]: "Warning",
+        [ProjectStatus.error]: "Critical",
+      };
 
       // get all project cards
       cy.get("main")
         .find("li")
         .each(($el, index) => {
           // check that project data is rendered
+          const status =
+            statusToText[mockProjects[index].status as ProjectStatus];
+
           cy.wrap($el).contains(mockProjects[index].name);
           cy.wrap($el).contains(languageNames[index]);
           cy.wrap($el).contains(mockProjects[index].numIssues);
           cy.wrap($el).contains(mockProjects[index].numEvents24h);
-          cy.wrap($el).contains(capitalize(mockProjects[index].status));
+          cy.wrap($el).contains(status);
           cy.wrap($el)
             .find("a")
             .should("have.attr", "href", "/dashboard/issues");
